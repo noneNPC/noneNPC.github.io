c8e43196e377d555d23c4d8267b18250aaf88b69
noneNPC <1472823422@qq.com>
2025-09-11 18:55:05 +0800

    Site updated: 2025-09-11 18:55:04

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/02/Ubuntu虚拟机与ROS1安装/","2025/09/11/全国大学生智能车创意组陕西理工大学校内选拔赛/","2025/04/02/小米路由器3G刷Breed以及OpenWrt/"];function toRandomPost(){
+var posts=["2025/04/02/小米路由器3G刷Breed以及OpenWrt/","2025/04/02/Ubuntu虚拟机与ROS1安装/","2025/09/11/全国大学生智能车创意组陕西理工大学校内选拔赛/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
