618a0e6064e538ecac9d32f26795f587edc0d099
noneNPC <1472823422@qq.com>
2025-11-02 21:25:52 +0800

    Site updated: 2025-11-02 21:25:52

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/10/22/2025全国大学生智能车创意组陕西理工大学校内选拔赛/","2025/09/11/2024全国大学生智能车创意组陕西理工大学校内选拔赛/","2025/04/02/Ubuntu虚拟机与ROS1安装/"];function toRandomPost(){
+var posts=["2025/09/11/2024全国大学生智能车创意组陕西理工大学校内选拔赛/","2025/10/22/2025全国大学生智能车创意组陕西理工大学校内选拔赛/","2025/04/02/Ubuntu虚拟机与ROS1安装/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
