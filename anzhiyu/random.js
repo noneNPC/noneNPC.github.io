568efd6938d2e3a0fb0f12a2d6296d3362cc3f93
noneNPC <1472823422@qq.com>
2025-12-10 22:10:05 +0800

    Site updated: 2025-12-10 22:10:04

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/11/04/2025全国大学生智能车创意组陕西理工大学校级选拔赛/","2025/09/11/2024全国大学生智能车创意组陕西理工大学校内选拔赛/","2025/04/02/Ubuntu虚拟机与ROS1安装/","2025/12/10/利用Clodflare-Tunnel实现无公网IP内网穿透/"];function toRandomPost(){
+var posts=["2025/09/11/2024全国大学生智能车创意组陕西理工大学校内选拔赛/","2025/11/04/2025全国大学生智能车创意组陕西理工大学校级选拔赛/","2025/04/02/Ubuntu虚拟机与ROS1安装/","2025/12/10/利用Clodflare-Tunnel实现无公网IP内网穿透/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
